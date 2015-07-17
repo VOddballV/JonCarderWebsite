f96ad9a41ff5cdacf3aa0f16e2e250e2d4b76fe6
Jon Carder <jcarder@access411.com>
2015-07-17 13:59:10 -0400

    Started using Angular and replaced the items above menu buttons with ng-repeat

---
+++ TestWebSite/Content/AngularApp.js
@@ -0,0 +1,35 @@
+/// <reference path="C:\Website\TestWebSite\Scripts/angular.min.js" />
+(function() {
+var app = angular.module('newSite', []);
+
+app.controller('StoreController', function () {
+    this.products = items;
+});
+
+var items = [
+    {
+        name: 'name1'
+    },
+    {
+        name: 'name1'
+    },
+    {
+        name: 'name1'
+    },
+    {
+        name: 'name1'
+    },
+    {
+        name: 'name1'
+    },
+    {
+        name: 'name1'
+    },
+    {
+        name: 'name1'
+    }
+];
+
+
+
+})();
